ae220ed86f98f689f019535f23564a0902743bdd
OKRAH DAUDA KINGSLEY(ODK) <99811118+daudaokrahkingsley@users.noreply.github.com>
2023-07-26 12:52:28 +0000

    Create SortingA.py

---
+++ SortingA.py
@@ -0,0 +1,23 @@
+# List of Integers
+numbers = [1, 3, 4, 2]
+
+# Sorting list of Integers
+numbers.sort()
+
+print(numbers)
+
+# List of Floating point numbers
+decimalnumber = [2.1, 2.00, 6.07, 3.28, 1.68]
+
+# Sorting list of Floating point numbers
+decimalnumber.sort()
+
+print(decimalnumber)
+
+# List of strings
+words = ["My", "Name", "ODK"]
+
+# Sorting list of strings
+words.sort()
+
+print(words)
